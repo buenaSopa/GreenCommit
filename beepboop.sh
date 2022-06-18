e303213552be471cbe987e76287b14cbfec6e1e8
sapnu-puas <bryankoh17102@gmail.com>
2022-06-18 14:09:42 +0800

    Vahala Biches

---
+++ beepboop.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# ------------------------------------------------------------------
+# [Bryan Kerr] GreenCommit
+#          Description: Make my github green again
+# ------------------------------------------------------------------
+
+SHA=$(curl https://api.github.com/repos/sapnu-puas/GreenCommit/contents/README.md | jq -r '.sha')
+#jq not download in heroku
+
+if [ -f .env ]; then
+    # Load Environment Variables
+    export $(cat .env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}' )
+fi
+
+curl \
+  -X PUT \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $GITTOKEN" \
+  https://api.github.com/repos/sapnu-puas/GreenCommit/contents/README.md \
+  -d "{\"message\":\"Wise man says only fool rush in\",\"committer\":{\"name\":\"McKidKneeKaneSir\",\"email\":\"$EMAIL\"},\"content\":\"TWNLaWRLbmVlS2FuZVNpcg==\",\"sha\":\"$SHA\"}"
