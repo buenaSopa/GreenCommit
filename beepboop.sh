be04cd6da49259edc995cf5374b031196c50b6d3
Gilbert <ngzhekai@gmail.com>
2023-02-08 10:04:34 +0800

    Update beepboop.sh

--- beepboop.sh
+++ beepboop.sh
@@ -19,4 +19,4 @@
 #   https://api.github.com/repos/sapnu-puas/GreenCommit/contents/README.md \
 #   -d "{\"message\":\"Wise man says only fool rush in\",\"committer\":{\"name\":\"McKidKneeKaneSir\",\"email\":\"$EMAIL\"},\"content\":\"TWNLaWRLbmVlS2FuZVNpcg==\",\"sha\":\"$SHA\"}"
 
-echo "Wise man says only fool rush in - McKidKneeKaneSir" > README.md
+echo "### Wise man says only fool rush in - McKidKneeKaneSir" > README.md
