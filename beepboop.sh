3101c268d34c20007981a03eec02d8e64897cd7c
Gilbert <ngzhekai@gmail.com>
2023-02-08 09:51:57 +0800

    Update beepboop.sh

--- beepboop.sh
+++ beepboop.sh
@@ -4,17 +4,19 @@
 #          Description: Make my github green again
 # ------------------------------------------------------------------
 
-SHA=$(curl https://api.github.com/repos/sapnu-puas/GreenCommit/contents/README.md | jq -r '.sha')
-#jq not download in heroku
+# SHA=$(curl https://api.github.com/repos/sapnu-puas/GreenCommit/contents/README.md | jq -r '.sha')
+# #jq not download in heroku
 
-if [ -f .env ]; then
-    # Load Environment Variables
-    export $(cat .env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}' )
-fi
+# if [ -f .env ]; then
+#     # Load Environment Variables
+#     export $(cat .env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}' )
+# fi
 
-curl \
-  -X PUT \
-  -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: token $GITTOKEN" \
-  https://api.github.com/repos/sapnu-puas/GreenCommit/contents/README.md \
-  -d "{\"message\":\"Wise man says only fool rush in\",\"committer\":{\"name\":\"McKidKneeKaneSir\",\"email\":\"$EMAIL\"},\"content\":\"TWNLaWRLbmVlS2FuZVNpcg==\",\"sha\":\"$SHA\"}"
+# curl \
+#   -X PUT \
+#   -H "Accept: application/vnd.github.v3+json" \
+#   -H "Authorization: token $GITTOKEN" \
+#   https://api.github.com/repos/sapnu-puas/GreenCommit/contents/README.md \
+#   -d "{\"message\":\"Wise man says only fool rush in\",\"committer\":{\"name\":\"McKidKneeKaneSir\",\"email\":\"$EMAIL\"},\"content\":\"TWNLaWRLbmVlS2FuZVNpcg==\",\"sha\":\"$SHA\"}"
+
+echo "Wise man says only fool rush in - McKidKneeKaneSir" > README.md
